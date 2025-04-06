2a6f7568c024c1d8a250ac5542f23cfb968b190c
economistblack <jack@economistblack>
2025-04-06 10:34:35 +0900

    update todolist 모델 1차 완료

--- todo_list_watchdog_app/lib/view/privatetodo.dart
+++ todo_list_watchdog_app/lib/view/privatetodo.dart
@@ -12,10 +12,10 @@ class PrivateToDo extends StatefulWidget {
 class _PrivateToDoState extends State<PrivateToDo> {
   // Property
   int userIndex = Get.arguments[0] ?? 0;
-  late String emailTypeId;
-  late String passKey;
-  late String profileImage;
-  late String nickName;
+  late String emailTypeId;  // 사용자 이메일 아이디
+  late String passKey;  // 사용자 비밀번호
+  late String profileImage; // 사용자가 선택한 프로파일 이미지
+  late String nickName; // 사용자가 선택한 닉네임
 
   @override
   void initState() {
@@ -42,24 +42,57 @@ class _PrivateToDoState extends State<PrivateToDo> {
       body: Column(
         children: [
           SizedBox(height: 70),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(''),
-              Column(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Color(0xFF38BDF8),
-                    child: ClipOval(
-                      child: Image.asset(profileImage, fit: BoxFit.contain),
-                    ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 250,
+                  height: 100,
+                  child: Row(
+                    children: [
+                      Icon(Icons.alarm_add_outlined, size: 40, color:  Color(0xFF38BDF8)),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text('data', ),
+                      ),
+                    ],
                   ),
-                  Text(nickName),
-                ],
-              ),
-            ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Color(0xFF334155),
+                          width: 2.0,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: 45,
+                        backgroundColor: Color(0xFF38BDF8),
+                        child: ClipOval(
+                          child: Image.asset(profileImage, fit: BoxFit.contain),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text(nickName),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: Divider(
+              color: Color(0xFFA3E635),
+            ),
+          )
         ],
       ),
     );
