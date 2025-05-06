5c8ec2e62767f1491793d7b22b7828980af03708
economistblack <jack@economistblack.org>
2025-05-06 16:26:18 +0900

    update

--- shoes_store_app_local/shoe_mart_app_jack/lib/view/product_display.dart
+++ shoes_store_app_local/shoe_mart_app_jack/lib/view/product_display.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:shoe_mart_app_jack/view/store_map.dart';
 
 class ProductDisplay extends StatefulWidget {
   const ProductDisplay({super.key});
@@ -8,8 +11,38 @@ class ProductDisplay extends StatefulWidget {
 }
 
 class _ProductDisplayState extends State<ProductDisplay> {
+  // Property
+  late String userId;
+  late String password;
+
+  final box = GetStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    userId = '';
+    password = '';
+    initStorage();
+  }
+
+  initStorage(){
+    userId = box.read('p_userId');
+    password = box.read('p_password');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$userId : $password'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Get.to(StoreMap());
+            }, 
+            icon: Icon(Icons.map))
+        ],
+      ),
+    );
   }
 }
